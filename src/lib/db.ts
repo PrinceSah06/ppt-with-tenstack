d1556709cc25ebd558018d5ee8463d55b2f0cf2d
prince sah <sahp4088@gmial.com>
2026-05-13 10:00:17 +0530

    first commit

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+import {PrismaClient} from "../generated/prisma/client"
+import {PrismaPg} from '@prisma/adapter-pg'
+// import type { PrismaClient } from '@prisma/client/extension';
+
+const adapter = new PrismaPg({
+    connectionString:process.env.DATABASE_URL,
+
+});
+
+declare global {
+    var __prisma : PrismaClient |undefined;
+}
+
+export const prisma = globalThis.__prisma || new PrismaClient({adapter});
+
+if(process.env.NODE_ENV !== 'production'){
+    globalThis.__prisma =prisma
+}
